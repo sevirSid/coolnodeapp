448025cc7a51529ce862e76b685cb3ced8cd5c54
sevirSid <sahmed1509@yahoo.fr>
2019-04-25 17:09:31 +0200

    Add files via upload

---
+++ src/app/hero.ts
@@ -0,0 +1,12 @@
+export class Message {
+  id: number;
+  value: string;
+  date: Date;
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
