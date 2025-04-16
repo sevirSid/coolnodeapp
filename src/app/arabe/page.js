ad3cf684a39268b9b516bf1fe2d48879d2f46fb9
sevirSid <sahmed1509@yahoo.fr>
2025-04-16 12:18:49 +0200

    Create page.js

---
+++ src/app/arabe/page.js
@@ -0,0 +1,13 @@
+// src/app/mosquee/page.js
+"use client"
+
+import React, { useState } from 'react';
+
+export default function Home() {
+
+  return (
+    <div>
+    <h1>Test pages</h1>
+    </div>
+  );
+}
